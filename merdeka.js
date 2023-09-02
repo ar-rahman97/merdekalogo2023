a2ebcad0f656c477dad863cead6592682704bb5d
Ar Rahman Mohamad <109963011+amanprogramm97@users.noreply.github.com>
2023-09-02 21:23:30 +0800

    first commit

---
+++ merdeka.js
@@ -0,0 +1,10 @@
+// const bucu = document.getElementById('bintang')
+// const bucubintang = 14
+// const sudutbintang = 360/bucubintang
+
+// for (let i=1; i<bucubintang+1; i++) {
+//     let newdiv = document.createElement("div")
+//     newdiv.className = "point"
+//     newdiv.style.transform = `rotate(${(i*sudutbintang).toFixed(3)}deg) translateX(-50%)`;
+//     bucu.appendChild(newdiv)
+// }
